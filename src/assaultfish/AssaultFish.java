decdd8b37b9a86e57c5440e3a9adbdd243c9ed7c
Eben Howard <eben.howard@gmail.com>
2014-03-08 14:01:35 -0700

    initial project commit

---
+++ src/assaultfish/AssaultFish.java
@@ -0,0 +1,16 @@
+package assaultfish;
+
+/**
+ *
+ * @author Eben Howard - http://squidpony.com - howard@squidpony.com
+ */
+public class AssaultFish {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
